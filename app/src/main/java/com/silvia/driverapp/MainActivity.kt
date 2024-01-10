b763492989d6890499e0c61c735c2b15060fb4b7
Silvia Rodríguez Bares <silvibares@gmail.com>
2024-01-10 20:38:14 +0100

    Initial commit

---
+++ app/src/main/java/com/silvia/driverapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.silvia.driverapp
+
+import android.R
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tl = findViewById<View>(R.id.main_table) as TableLayout
+
+        val tr_head = TableRow(this)
+        tr_head.id = 10
+        tr_head.setBackgroundColor(Color.GRAY) // part1
+
+        tr_head.layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
+        )
+    }
+}
